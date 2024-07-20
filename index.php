2312fab1e18c69173d796a05ac081d889f21aa36
zainbondoq <zainbondoq@gmail.com>
2024-07-20 17:06:43 +0300

    oop-video4

--- index.php
+++ index.php
@@ -1,47 +1,37 @@
 <?php
 
-class User {
-    private $name;
-    private $age;
 
-    public function __construct($name , $age){
+class User {
+ 
+    protected $name = 'Brad';
+    protected $age;
+    
+    
+    public function __construct($name , $age) {
         $this->name = $name;
         $this->age = $age;
     }
-
-
-    // public function getName(){
-    //     return $this->name;
-    // }
-
-    // public function setName($name){
-    //     $this->name = $name;
-    // }
-
-    //__get MAGIC METHOD
-
-    public function __get($property){
-        if(property_exists($this , $property)){
-            return $this->$property;
-        }
-    }
-
-    public function __set($property , $value){
-        if(property_exists($this , $property)){
-            $this->$property = $value;
-        }
-        return $this;
-    }
 }
 
+class Customer extends User{
+    private $balance;
 
+    public function __construct($name , $age ,$balance) {
+        parent::__construct($name , $age);
+        $this->balance = $balance;
+    }
 
-$user1 = new User('John' , 40); // this withh thorw a fatal error thats why we need setters and getters
+    public function pay($amount){
+        return $this->name .'paid $ ' . $amount;
+    }
 
-$user1->__set('age' , 41);
+    public function getBalance(){
+        return $this->balance;
+    }
 
-echo $user1->__get('name');
-echo $user1->__get('age');
+}
 
-// echo $user1->setName('zain'); 
-// echo $user1->getName();
+$customer1 = new Customer('ZAIN' , 250 ,500);
+//echo $customer1->pay(100);
+
+echo $customer1->getBalance();
