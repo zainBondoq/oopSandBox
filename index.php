0db097cefaab0c933b123a0c0b26b491338c2c64
zainbondoq <zainbondoq@gmail.com>
2024-07-20 16:25:08 +0300

    oop-video2

--- index.php
+++ index.php
@@ -1,23 +1,36 @@
 <?php
 
 class User {
+    
     public $name;
     public $age;    
+
+
+    //runs when the obj is created!!
+public function __construct($name , $age){
+
+    //__CLASS__ ==> RETURNS THE NAME OF THE CLASS IM STANDING IN !
+    echo __CLASS__ . '  class <br>';
+    $this->name = $name;
+    $this->age = $age;
+}
+
+
+
+//called when no other refrences to a certain obj
+//used for cleanup, closing connections ,etc..
+public function __destruct(){
+    echo 'destruct...';
+}
+
+
 
     public function sayHello(){
         return 'Name : ' . $this->name . ' , '.'age : ' . $this->age . 'says hello' ;
     }
 }
 
-$user1 = new User();
-$user1->name = 'zain';
-$user1->age = '21';
-echo $user1->sayHello();
+$user1 = new User('zainBondoq' , 21);
 
-echo '<br>';
-$user2 = new User();
-$user2->name = 'lujain';
-$user2->age = '21';
 
-echo $user2->sayHello();
-
+echo $user1->name . ' is ' . $user1->age . ' years old <br>';
